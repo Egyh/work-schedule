fd23e91744b618a68259475b5ebadc6abb09b357
Hamaguchi <hamaguchi.fundely@gmail.com>
2024-02-02 19:02:37 +0900

    add: NamePlateのコンポーネント化

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 'use client';
 import Header from "@/components/Header";
+import NamePlate from "@/components/user/NamePlate";
+import Form from "@/components/user/form";
 import Footer from "@/components/Footer";
+
+const NamePlate = {
+	name: "Hamaguchi",
+	content: "TODO内容はここに記載します。",
+};
 
 export default function Home() {
 	return (
     <>
 		<Header/>
+    <NamePlate {...NamePlate} />
+    <Form/>
     <Footer/>
     </>
 	);
